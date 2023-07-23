cee631256c2e6713d2347aa8634b35b1903ff508
Joynal Abedin <cfcfahim26@gmail.com>
2023-07-23 21:52:36 +0600

    update on auth api

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -2,7 +2,7 @@ import connectToDB from '@utils/database';
 import Prompt from '@models/prompt';
 
 export const POST = async (req, res) => {
-  const { userId, prompt, tag } = await res.json();
+  const { userId, prompt, tag } = await req.json();
 
   try {
     await connectToDB();
@@ -12,6 +12,7 @@ export const POST = async (req, res) => {
 
     return new Response(JSON.stringify(newPrompt), { status: 201 });
   } catch (error) {
+    console.log(error);
     return new Response('Failed to create a new prompt', { status: 500 });
   }
 };
