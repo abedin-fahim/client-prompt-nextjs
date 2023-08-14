5e714b879d2adc09f8b6fe77692888ba68d3186d
Joynal Abedin <cfcfahim26@gmail.com>
2023-08-14 23:15:26 +0600

    profile added

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+'use client';
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import Profile from '@components/Profile';
 
-const Profile = () => {
-  return <div>Profile</div>;
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const prompts = await response.json();
+
+      setPosts(prompts);
+    };
+    if (session?.user.id) {
+      fetchPost();
+    }
+  }, [session?.user.id]);
+
+  const handleEdit = () => {};
+  const handleDelete = () => {};
+  return (
+    <Profile
+      name='My'
+      desc='Welcome to your personal profile'
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
