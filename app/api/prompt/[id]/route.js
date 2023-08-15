c6a13dc4cfc3832f7a91432cdefed5af68bbec17
Joynal Abedin <cfcfahim26@gmail.com>
2023-08-15 21:43:08 +0600

    edit delete added

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,25 @@
+import connectToDB from '@utils/database';
+import Prompt from '@models/prompt';
+
+// GET
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    const prompt = await Prompt.findById(params.id).populate('creator');
+    if (!prompt) {
+      return new Response('Prompt not found!', { status: 404 });
+    }
+
+    return new Response(JSON.stringify(prompt), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response('Failed to fetch the prompts', {
+      status: 500,
+    });
+  }
+};
+
+// PATCH
+// DELETE
